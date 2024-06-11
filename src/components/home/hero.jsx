8b88b7054b4832228f87ea2a359a83528051902d
ekhanal <elinakhanal467@gmail.com>
2024-06-11 11:28:07 +0545

    change

--- src/components/home/hero.jsx
+++ src/components/home/hero.jsx
@@ -1,5 +1,5 @@
 import HeroImage from "../../assets/images/heroimage.jpg";
-import { useState } from "react";
+import {  useState } from "react";
 import { RiHeart2Line } from "react-icons/ri";
 import { RiMoneyDollarBoxLine } from "react-icons/ri";
 import { SiDreamstime } from "react-icons/si";
@@ -9,9 +9,12 @@ import { TbFriends } from "react-icons/tb";
 import { GiThreeFriends } from "react-icons/gi";
 import { FaPerson } from "react-icons/fa6";
 import { SiAnytype } from "react-icons/si";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const Hero = () => {
-  const [selectedButton, setSelectedButton] = useState(false);
+  const [selectedButton, setSelectedButton] = useState("Travels");
+
   const data = {
     Travels: [
       {
@@ -94,7 +97,7 @@ const Hero = () => {
         className="relative bg-cover bg-center min-h-[40vh] sm:min-h-screen  "
         style={{ backgroundImage: `url(${HeroImage})` }}
       >
-        <div className="flex flex-col items-center h-full md:justify-center absolute w-full bg-black bg-opacity-50 ">
+        <div className="flex flex-col items-center h-full md:justify-center absolute w-full  bg-opacity-50 ">
           <h1 className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl border-b-2 border-yellow-500 pb-6 px-4 text-center pt-4 ">
             Find your Holiday
           </h1>
@@ -103,9 +106,9 @@ const Hero = () => {
               return (
                 <button
                   key={index}
-                  className={` ${
+                  className={` text-base sm:text-lg md:text-xl lg:text-2xl py-2 px-4 rounded focus:outline-none ${
                     selectedButton == item ? "text-yellow-500" : "text-white"
-                  }text-base sm:text-lg md:text-xl lg:text-2xl py-2 px-4 rounded focus:outline-none  `}
+                  } `}
                   onClick={() => {
                     setSelectedButton(item);
                   }}
